b68fbffd888469d7843307f1ecf2e4c56e0e4892
Xtian <xtiancuenta1@gmail.com>
2022-08-16 19:34:14 -0300

    Se agregó un get para listar las personas

--- src/main/java/com/example/retofuncyreact/controller/PersonController.java
+++ src/main/java/com/example/retofuncyreact/controller/PersonController.java
@@ -1,11 +1,14 @@
 package com.example.retofuncyreact.controller;
 
+import com.example.retofuncyreact.model.Person;
 import com.example.retofuncyreact.repository.IPersonRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/persons")
@@ -14,4 +17,9 @@ public class PersonController {
 
     @Autowired
     private IPersonRepository personRepository;
+
+    @GetMapping
+    public Flux<Person> list(){
+        return personRepository.list();
+    }
 }
