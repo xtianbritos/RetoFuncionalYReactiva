723b4affb2eceed607aba241bf46c4cd608c56f6
Xtian <xtiancuenta1@gmail.com>
2022-08-16 19:38:19 -0300

    Se agregó un post para registrar personas

--- src/main/java/com/example/retofuncyreact/controller/PersonController.java
+++ src/main/java/com/example/retofuncyreact/controller/PersonController.java
@@ -5,10 +5,9 @@ import com.example.retofuncyreact.repository.IPersonRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/persons")
@@ -21,5 +20,10 @@ public class PersonController {
     @GetMapping
     public Flux<Person> list(){
         return personRepository.list();
+    }
+
+    @PostMapping
+    public Mono<Person> regist(@RequestBody Person p){
+        return personRepository.regist(p);
     }
 }
